10f96c183c3305826f7f11793d2de02c9a4add5e
albertkun <albertk@gmx.com>
2024-07-29 04:28:06 -0700

    fix: missing build npm module

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,23 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'production', // Set the mode to 'production' for optimized builds
-    entry: './src/main.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            }
-        ]
-    }
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
